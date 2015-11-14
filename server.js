d9947f4c21b700f80ed976934a8f10b245343c73
Grady Weber <grady.weber@gmail.com>
2015-11-14 14:48:27 -0500

    This is a working website skeleton, uses Express.js. Build with npm install, run with npm run

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log("test");
+var http = require('http');
+var express = require('express');
+var app = express();
+app.use(express.static('public'));
+
+var server = app.listen(8000, function(){
+    console.log('Server Started at port 8000');  
+});
+process.on('SIGTERM', function(){
+    process.exit();
+});
