9972df7d394b7d856cbc122420fc028ceff865f6
David Zhou <davidzhou94@gmail.com>
2015-11-15 17:09:29 -0500

    Added view changing prototype

--- public/js/core.js
+++ public/js/core.js
@@ -1,16 +1,62 @@
 // declare a module
-var teamsModule = angular.module('teams', []);
+var fantasyControllers = angular.module('fantasyControllers', []);
 
-function mainController($scope, $http) {
-  $scope.formData = {};
+fantasyControllers.controller('leagueController', ['$scope', '$http', '$routeParams', 
+  function ($scope, $http, $routeParams) {
+    $scope.formData = {};
 
-  // when landing on the page, get all teams and show them
-  $http.get('/api/teams')
-    .success(function(data) {
-      $scope.teams = data;
-      console.log(data);
-    })
-    .error(function(data) {
-      console.log('Error: ' + data);
-    });
-}
+    // when landing on the page, get all teams and show them
+    $http.get('/api/teams/' + $routeParams.leagueid)
+      .success(function(data) {
+        $scope.teams = data;
+        console.log(data);
+      })
+      .error(function(data) {
+        console.log('Error: ' + data);
+      });
+}]);
+
+fantasyControllers.controller('homeController', ['$scope', '$http', '$location', 
+  function ($scope, $http, $location) {
+    $scope.go = function ( path ) {
+      console.log("fuck this");
+      $location.path( path );
+    };
+    $scope.formData = {};
+
+    // when landing on the page, get all teams and show them
+    $http.get('/api/leagues')
+      .success(function(data) {
+        $scope.leagues = data;
+        console.log(data);
+      })
+      .error(function(data) {
+        console.log('Error: ' + data);
+      });
+}]);
+
+var fantasyApp = angular.module('fantasyApp', [
+  'ngRoute',
+  'fantasyControllers'
+]);
+
+fantasyApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/team', {
+        templateUrl : 'team.html',
+        controller : 'teamController'
+      }).
+      when('/league/:leagueid', {
+        templateUrl : 'league.html',
+        controller : 'leagueController'
+      }).
+      when('/home', {
+        templateUrl : 'home.html',
+        controller : 'homeController'
+      }).
+      otherwise({
+        redirectTo : '/home'
+      });
+  }
+]);
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var app = express();
 var morgan = require('morgan');             // log requests to the console (express4)
 var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
 var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+var path = require('path');
 
 app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
 app.use(morgan('dev'));                                         // log every request to the console
@@ -12,18 +13,39 @@ app.use(bodyParser.json());                                     // parse applica
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride());
 
-app.get('/api/teams', function(req, res) {
-  // hardcoded list of todos for now
+app.get('/api/teams/:league', function(req, res) {
+  // hardcoded list of teams for now
+  var gameid = req.params.league;
+  if (gameid == 1) {
+    res.json(
+    [
+      {id : 1, text : "Team Mr. Poopybutthole"},
+      {id : 2, text : "Team Mr. Meeseeks"}
+    ]);
+  } else {
+    res.json(
+    [
+      {id : 1, text : "Team Morty"},
+      {id : 2, text : "Team Rick"},
+      {id : 3, text : "Team Summer"},
+      (id : 4, text : "Team Beth"},
+      {id : 5, text : "Team Jerry AKA Why do I have to be last!?!"}
+    ]);
+  }
+});
+
+app.get('/api/leagues', function(req, res) {
+  // hardcoded list of leagues for now
   res.json(
   [
-    {id : 1, text : "Mr. Poopybutthole"},
-    {id : 2, text : "Mr. Meeseeks"}
+    {id : 1, text : "League 1"},
+    {id : 2, text : "League 2"}
   ]);
 });
 
 app.get('*', function(req, res) {
   // load the single view file (angular will handle the page changes on the front-end)
-  res.sendFile('./public/index.html');
+  res.sendFile(path.join(__dirname, './public', 'index.html'));
 });
 
 var server = app.listen(8000, function(){
